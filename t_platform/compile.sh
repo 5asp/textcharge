7e55a863757e162567cffb21ce18a07bf097b48a
xia <xiajinxue@sharkgulf.com>
2023-11-29 17:49:50 +0800

    t_platform

--- t_platform/compile.sh
+++
@@ -1,4 +0,0 @@
-# protoc --proto_path=pb --go_opt=appinfo.proto=github.com/aheadIV/textcharge/t_platform/pb 
-
-
-protoc --proto_path=proto --go_out=pb --go_opt=paths=source_relative proto/app/info.proto
